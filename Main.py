74085de316afd408a75e3e021b7359dafafe950e
GHW0826 <tjdtn7745@gmail.com>
2024-11-15 20:17:27 +0900

    기본 티커 조회 테스트

--- Main.py
+++ Main.py
@@ -1,1 +1,24 @@
+from configparser import ConfigParser
+import pyupbit
+
+
+config = ConfigParser()
+config.read('config.ini')
+
+access_key = config['secret']['access_key']
+secret_key = config['secret']['secret_key']
+
+print(access_key)
+print(secret_key)
 print("Hello World")
+
+### 거래할 코인 symbol
+coin = "KRW-XRP" 
+
+
+### 업비트 연동
+upbit = pyupbit.Upbit(access_key, secret_key)
+
+tickers = pyupbit.get_tickers()
+print(tickers)
+print(type(tickers))
